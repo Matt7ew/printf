e597cadfb5ddfaea5dc4fc632123c2d0b58eb3ae
Matt7ew <123404854+Matt7ew@users.noreply.github.com>
2025-05-17 02:11:26 +0200

    formated ft_pharse_format file

--- ft_printf_fl.c
+++ ft_printf_fl.c
@@ -3,39 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf_fl.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: matthewjorge <matthewjorge@student.42.f    +#+  +:+       +#+        */
+/*   By: mjorge <mjorge@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 16:44:03 by matthewjorg       #+#    #+#             */
-/*   Updated: 2025/05/06 09:33:15 by matthewjorg      ###   ########.fr       */
+/*   Updated: 2025/05/17 02:11:01 by mjorge           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int parse_format(const char *str, va_list args)
+static int	parse_format(const char *str, va_list args)
 {
-    int count = 0;
-    int i = 0;
+	int	count;
+	int	i;
 
-    while (str[i])
-    {
-        if (str[i] == '%')
-        {
-            i++; // Move past '%'
-            if (!str[i]) // Edge case: ends with %
-                break;
-            count = handle_specifier(str[i], args, count);
-            if (count == -1)
-                return (-1); // Propagate write error
-        }
-        else
-        {
-            if (write(1, &str[i], 1) == -1)
-                return (-1);
-            count++;
-        }
-        i++;
-    }
-    return (count);
+	count = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			if (!str[i])
+				break ;
+			count += handle_specifier(str[i], args);
+			if (count == -1)
+				return (-1);
+		}
+		else
+		{
+			if (write(1, &str[i], 1) == -1)
+				return (-1);
+			count++;
+		}
+		i++;
+	}
+	return (count);
 }
-
