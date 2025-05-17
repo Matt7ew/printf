c60ab870ef75bd956772c60b2e749277941e3cb6
Matt7ew <123404854+Matt7ew@users.noreply.github.com>
2025-05-17 02:09:12 +0200

    formated ft_xprint file

--- ft_bigX.c
+++ ft_bigX.c
@@ -6,7 +6,7 @@
 /*   By: mjorge <mjorge@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 13:53:12 by matthewjorg       #+#    #+#             */
-/*   Updated: 2025/05/17 01:45:35 by mjorge           ###   ########.fr       */
+/*   Updated: 2025/05/17 02:08:52 by mjorge           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,12 @@
 int	ft_print_BX(unsigned int s)
 {
 	char	*str;
-	int 	rl;
-
+	int		rl;
 
 	str = ft_strbase(s, "0123456789ABCDEF");
-	if (str == NULL)
+	if (!str)
 		return (-1);
-	if (ft_putstr(str) < 0)
-	{
-		free(str);
-		return (-1);
-	}
-	rl = ft_strlen(str);
+	rl = ft_putstr(str);
 	free(str);
 	return (rl);
 }
@@ -34,18 +28,12 @@ int	ft_print_BX(unsigned int s)
 int	ft_print_sx(unsigned int s)
 {
 	char	*str;
-	int 	rl;
-
+	int		rl;
 
 	str = ft_strbase(s, "0123456789abcdef");
-	if (str == NULL)
+	if (!str)
 		return (-1);
-	if (ft_putstr(str) < 0)
-	{
-		free(str);
-		return (-1);
-	}
-	rl = ft_strlen(str);
+	rl = ft_putstr(str);
 	free(str);
 	return (rl);
 }
